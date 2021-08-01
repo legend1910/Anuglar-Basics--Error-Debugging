2e3e64b02221d4bafb6dcfabc7cc08af3d31444d
legend1910 <legend.yuva@gmail.com>
2021-08-01 15:56:18 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,13 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  Servers;
+  Servers: string[] = [];
   name = 'Angular ' + VERSION.major;
   onAddServer() {
     this.Servers.push('Another Server');
   }
+
+  // This will throw an error where you are trying to delete the last file, you  can debug and see on the source->mai bundle.js which on putting breakpoint  will show the ts files using sourcemap
   onRemoveServer(id: number) {
     const position = id + 1;
     this.Servers.splice(position, 1);
